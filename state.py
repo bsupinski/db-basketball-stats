9bec35b22337826cff2f853de7893025b47a255c
Brandon Supinski <66806979+bsupinski@users.noreply.github.com>
2024-06-07 16:29:11 -0400

    started sort team functionality

--- state.py
+++ state.py
@@ -33,13 +33,9 @@ def clean_list(list):
     return list
 
 
-def team_guardians(list):
-    guardians = []
-    for player in list:
-        guardians.append(player['guardians'])
-    print(", ".join(guardians))
-
-
+def list_names(list, key):
+    name = [player[key] for player in list]
+    print(", ".join(name))
 
 
 
@@ -62,13 +58,27 @@ def get_avg_height(list):
     print(round(total_height / len(list), 1))
 
 
+def filter_by_exp(list):
+    filtered_players = [[], []]
+    [filtered_players[0].append(player)  if player['experience'] == True else filtered_players[1].append(player) for player in list]
+    return(filtered_players)
 
+def create_teams(list):
+    teams =[[], [], []]
+    for player in list[0]:
+        if len(teams[0]) < len(teams[1]):
+            teams[0].append(player)
+        elif len(teams[1]) < len(teams[2]):
+            teams[1].append(player)
+        else:
+            teams[2].append(player)
+    print(len(teams[0]), len(teams[1]), len(teams[2]))
 
 clean_player_list = clean_list(copy.deepcopy(PLAYERS))
 
 
 
 if __name__ == "__main__":
-    get_avg_height(clean_player_list)
+    create_teams(filter_by_exp(clean_player_list))
     pass
         
